c2caf2d0c9528e6a773b1cf6d7f5430d9b04d1eb
Gasic <gasic@cleansoft.rs>
2021-12-21 14:26:33 +0100

    add dummy tests

---
+++ tests/SomeTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Money\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SomeTest extends TestCase
+{
+    public function testA()
+    {
+        self::assertSame(1, 1);
+    }
+}
